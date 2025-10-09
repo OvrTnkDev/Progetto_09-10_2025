e1ffbc27cd7e8de20ad3bb573d5d878b04fb270e
Arvin <arvin.nikzad@gmail.com>
2025-10-09 16:51:34 +0200

    update on juornal

--- Gestione di un giornale/GestioneGiornale/Utils/Header.cs
+++ Gestione di un giornale/GestioneGiornale/Utils/Header.cs
@@ -3,7 +3,13 @@ namespace Utils
     // Classe specializzata per gestire emergenze
     public class Header : Journal
     {
-     public Header() : base("Daily news", 1000){}
+        public string Title { get; set; }
+        public Header(string title)
+        {
+            Title = title;
+        }
+
+        public Header() : base("Daily news", 1000){}
         public override void PublishSection()
         {
             Console.WriteLine("Header section: Highlights ");
