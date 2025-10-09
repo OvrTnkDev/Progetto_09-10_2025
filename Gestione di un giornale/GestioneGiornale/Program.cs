1a85da6db2a11756a3bbdd34af27079c8574ec54
OvrTnkDev <dalessandrofabio96@gmail.com>
2025-10-09 15:30:29 +0200

    CreazioneSolution
    Abbiamo inizializzato il progetto con la creazione della Solution e delle cartelle

---
+++ Gestione di un giornale/GestioneGiornale/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+//using Utils;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
