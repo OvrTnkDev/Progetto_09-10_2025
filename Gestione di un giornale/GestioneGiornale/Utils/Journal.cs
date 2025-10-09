e5bf1014b4c098a90d81e6865fd93834ce8b6e72
OvrTnkDev <dalessandrofabio96@gmail.com>
2025-10-09 16:25:20 +0200

    Classe Journal
    Ho creato la prima classe Journal

--- Gestione di un giornale/GestioneGiornale/Utils/Journal.cs
+++ Gestione di un giornale/GestioneGiornale/Utils/Journal.cs
@@ -1,10 +1,17 @@
+// Utils/Journal.cs
 using System;
+using System.Collections.Generic;
 
 namespace Utils
 {
-    // Classe generica di un operatore
     public class Journal
     {
-        
+        public DateTime Data { get; set; } = DateTime.Today;
+        public List<string> Articoli { get; set; } = new List<string>();
+
+        public virtual void StampaBase()
+        {
+            Console.WriteLine($"Giornale del {Data:dd/MM/yyyy}");
+        }
     }
 }
