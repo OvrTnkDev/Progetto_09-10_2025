528f0b558f7fe663f21f80e46738403a3443374e
OvrTnkDev <dalessandrofabio96@gmail.com>
2025-10-09 17:56:40 +0200

    1

--- Gestione di un giornale/GestioneGiornale/obj/Debug/net9.0/GestioneGiornale.AssemblyInfo.cs
+++ Gestione di un giornale/GestioneGiornale/obj/Debug/net9.0/GestioneGiornale.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestioneGiornale")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22da4fbb366601dbe25bb114a5d839515afe0765")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+453325f426d3002f93aa2675e339f252199d3804")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestioneGiornale")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestioneGiornale")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
