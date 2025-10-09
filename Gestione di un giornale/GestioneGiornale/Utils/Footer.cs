c9610e3e6036395f9e4a458f3cb69fcba87f5122
OvrTnkDev <dalessandrofabio96@gmail.com>
2025-10-09 17:04:01 +0200

    Push classe Footer

--- Gestione di un giornale/GestioneGiornale/Utils/Footer.cs
+++ Gestione di un giornale/GestioneGiornale/Utils/Footer.cs
@@ -5,5 +5,37 @@ namespace Utils
     // Classe specializzata per gestire la sicurezza
     public class Footer : Center
     {
+        public string Redazione { get; set; }
+        // valore default
+        private string _direttore = "Non specificato";
+        public string Direttore
+        {
+            get => _direttore;
+            set
+            {
+                if (!string.IsNullOrEmpty(value)) { _direttore = value; }
+                else { Console.WriteLine($"Non hai inserito il Direttore"); }
+            }
+        }
+        public string Copyright { get; set; }
+
+        public Footer(string sottotitolo, string corpo, string redazione, string direttore, string copyright)
+                     : base(sottotitolo, corpo)
+        {
+            Redazione = redazione;
+            Direttore = direttore;
+            Copyright = copyright;
+        }
+
+        public override void StampaInfo()
+        {
+            base.StampaInfo();
+            //Console.WriteLine($"Titolo: {Titolo}, Copie Totali: {CopieTotali}");
+            Console.WriteLine($"Redazione: {Redazione}\t" +
+                              $"Direttore: {Direttore}\t" +
+                              $"Copyright: {Copyright}");
+            Console.WriteLine();
+        }
+
     }
 }
