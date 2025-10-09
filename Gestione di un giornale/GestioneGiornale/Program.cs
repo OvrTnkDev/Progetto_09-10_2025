b2406b2e776d36f4cdaffc2d2d18aa8a9909683a
Arvin <arvin.nikzad@gmail.com>
2025-10-09 17:02:47 +0200

    update main

--- Gestione di un giornale/GestioneGiornale/Program.cs
+++ Gestione di un giornale/GestioneGiornale/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-//using Utils;
+using Utils;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello!");
+        Random random = new Random();
+        for( int day =1; day<=5; day++)
+        { }
+
     }
 }
--- Gestione di un giornale/GestioneGiornale/obj/Debug/net9.0/GestioneGiornale.AssemblyInfo.cs
+++ Gestione di un giornale/GestioneGiornale/obj/Debug/net9.0/GestioneGiornale.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestioneGiornale")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a85da6db2a11756a3bbdd34af27079c8574ec54")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38d449caf3b8d19944cc61fbd34e3e54342b6936")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("GestioneGiornale")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestioneGiornale")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
