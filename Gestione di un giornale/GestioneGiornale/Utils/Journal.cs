774340c21119802705665e357ebc0397b88e9524
Arvin <arvin.nikzad@gmail.com>
2025-10-09 16:23:28 +0200

    journal class

--- Gestione di un giornale/GestioneGiornale/Utils/Journal.cs
+++ Gestione di un giornale/GestioneGiornale/Utils/Journal.cs
@@ -5,6 +5,16 @@ namespace Utils
     // Classe generica di un operatore
     public class Journal
     {
-        
+        public string Titolo { get; set; }
+        public int CopieTotali { get; set; }
+        public void journal(string Titolo, int CopieTotali)
+        {
+            this.Titolo = Titolo;
+            this.CopieTotali = CopieTotali;
+        }
+        public virtual void StampaInfo()
+        {
+            Console.WriteLine($"Titolo: {Titolo}, Copie Totali: {CopieTotali}");
+        }
     }
 }
